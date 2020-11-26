3e1b98c873365b0d7c83d7085e8d3002685e7e95
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2020-11-26 22:30:59 +0530

    Add files via upload

---
+++ 2.DataTypes.java
@@ -0,0 +1,70 @@
+package com.lainlimbo;
+
+import java.util.*;
+
+import java.util.stream.IntStream;
+
+public class DataTypes {
+// data type is how much memory we are storing aside for a value
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		//byte, short, char, boolean, int, float
+		//double, long
+		
+		//to find the range of a data type use MAX_VALUE and MIN_VALUE
+
+		System.out.println("Byte Max:" + Byte.MAX_VALUE);
+		System.out.println("Byte Min:" + Byte.MIN_VALUE);
+		System.out.println("Short Max:" + Short.MAX_VALUE);
+		System.out.println("Short Min:" + Short.MIN_VALUE);
+		System.out.println("Char Max:" + (Character.MAX_VALUE+0));
+		System.out.println("Char Min:" + (Character.MIN_VALUE+0));
+		System.out.println("Int Max:" +	Integer.MAX_VALUE);
+		System.out.println("Int Min:" +	Integer.MIN_VALUE);
+		System.out.println("Float Max:" + Float.MAX_VALUE);
+		System.out.println("Float Min:" + Float.MIN_VALUE);
+		System.out.println("Double Max:" + Double.MAX_VALUE);
+		System.out.println("Double Min:" + Double.MIN_VALUE);
+		System.out.println("Long Max:" + Long.MAX_VALUE);
+		System.out.println("Long Min:" + Long.MIN_VALUE);
+	
+		//use + in println when want to add more things after " "
+				
+		boolean happy = true;
+		//value either true or false
+		//can't use 0 or 1 like in other languages
+		
+		char a = 'a';
+		//only save single character
+		//char can also save below things too
+		// \n new line \t tab \b backspace \f form feeds \r returns
+		// \" double quote \' single quote \\ back slash
+		
+		float fNUM = 1.645245665464656F;
+		float fNUM2 = 1.8838758375535F;
+		//precision = 6 decimal places
+		//use F to denote it as a float
+		
+		System.out.println("FLoat : " + (fNUM + fNUM2));
+		
+		double dblNUM = 1.645245665464656;
+		double dblNUM2 = 1.8838758375535;
+		//precision upto 15 decimal places
+		
+		System.out.println("Double : " + (dblNUM + dblNUM2));
+		
+		double thousand = 1e+3;
+		System.out.print(thousand);
+		//can also use scientific notation
+		
+		long bigNUM = 123_456_789;
+		//use underscore to write big numbers
+		
+	
+	}
+	
+
+}
